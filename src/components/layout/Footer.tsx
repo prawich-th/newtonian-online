ea73da0819064ffe5fb30201a01e24403735c411
SEN-SENSATION <prawich.th@gmail.com>
2022-09-16 02:10:10 +0700

    vercel, alpha

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       </div>
 
       <div className="footer__right">
-        <h4>The Newtonian Online v1.0</h4>
+        <h4>The Newtonian Online (Alpha)</h4>
       </div>
     </div>
   );
