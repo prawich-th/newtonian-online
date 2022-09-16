11379799ebfa8977d465a36e69b0b235225d1f29
SEN-SENSATION <prawich.th@gmail.com>
2022-09-16 21:46:18 +0700

    media-query

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ const article = {
     id: "87yhjsm39dkfdis83ndso",
     title: "Oam and Shang: The two great minds behind the Newton Musical",
     text: "Oam and Shang are the two lead directors and founders of the Newton Musical, an event that will occur within this school year (AY 2022/23), showcasing multiple performances of a musical inspired by Dear Evan Hansen. This will be the first musical to ever be performed and created by the Newton Sixth Form student body, and they have recruited a talented group of actors to portray the different characters in the musical. One of the performances will be at the Siam Pic-Ganesha, the largest theater in Thailand, on December 24th, 2022.",
-    image: "/shang-oam.jpg",
+    image: "/shang-oam.png",
   },
 };
 
